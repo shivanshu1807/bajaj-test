e0fefc8921ffbf65c0e4888b0ab1fba96474fa76
Shivanshu Singh <sr5752@srmist.edu.in>
2024-09-22 16:54:12 +0530

    new

--- Backend/index.js
+++ Backend/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const bfhlRoute = require('./routes/bfhlRoute');
+const cors = require('cors');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -8,6 +9,7 @@ const port = process.env.PORT || 3000;
 // Middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors);
 
 // Routes
 app.use('/bfhl', bfhlRoute);
